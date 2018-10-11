1c1cbcb9853722b5cfa1c470faadbc190d2499fc
Ka-Wai Lin <kwlin@users.noreply.github.com>
2018-10-11 10:54:52 +0200

    Fix build & namespace.

--- Dependency Inversion Principle/DependencyInversion.Implementation.Refactor/Repository/IGebruikerRepository.cs
+++ Dependency Inversion Principle/DependencyInversion.Implementation.Refactor/Repository/IGebruikerRepository.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace DependencyInversion.Implementation.Refactor
+namespace DependencyInversion.Implementation.Refactor.Repository
 {
     public interface IGebruikerRepository
     {
